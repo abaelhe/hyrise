195e2cfc81e0ac896d28a4041fa8452a3c4c781f
Julian Menzler <julianmenzler@gmail.com>
2020-08-26 11:41:24 +0200

    Try Fix

--- src/lib/optimizer/strategy/create_fds.cpp
+++ src/lib/optimizer/strategy/create_fds.cpp
@@ -1,6 +1,7 @@
 #include "create_fds.hpp"
 
 #include "logical_query_plan/abstract_lqp_node.hpp"
+#include "logical_query_plan/abstract_non_query_node.hpp"
 
 namespace opossum {
 
@@ -8,8 +9,10 @@ void CreateFDsRule::apply_to(const std::shared_ptr<AbstractLQPNode>& lqp) const 
 
   for(size_t i = 0; i < 1000; ++i) {
     Assert(!lqp->right_input(), "Did not expect right input node");
-    const auto& fds = lqp->left_input()->functional_dependencies();
-    Assert(fds.size() > 0 || fds.size() == 0, "Dummy usage");
+    if(!dynamic_pointer_cast<AbstractNonQueryNode>(lqp->left_input())) {
+      const auto& fds = lqp->left_input()->functional_dependencies();
+      Assert(fds.size() > 0 || fds.size() == 0, "Dummy usage");
+    }
   }
 
 }
--- src/lib/optimizer/strategy/create_unique_constraints.cpp
+++ src/lib/optimizer/strategy/create_unique_constraints.cpp
@@ -8,8 +8,10 @@ void CreateUniqueConstraintsRule::apply_to(const std::shared_ptr<AbstractLQPNode
 
   for(size_t i = 0; i < 1000; ++i) {
     Assert(!lqp->right_input(), "Did not expect right input node");
-    const auto& unique_constraints = lqp->left_input()->unique_constraints();
-    Assert(unique_constraints->size() > 0 || unique_constraints->size() == 0, "Dummy usage");
+    if(!dynamic_pointer_cast<AbstractNonQueryNode>(lqp->left_input())) {
+      const auto& unique_constraints = lqp->left_input()->unique_constraints();
+      Assert(unique_constraints->size() > 0 || unique_constraints->size() == 0, "Dummy usage");
+    }
   }
 
 }
