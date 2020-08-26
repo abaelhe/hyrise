bf7d85cb794830556edab2626c03ef0ae653cae6
Julian Menzler <julianmenzler@gmail.com>
2020-08-26 11:33:54 +0200

    Try fix & increase to 1000 iterations

--- src/lib/optimizer/strategy/create_fds.cpp
+++ src/lib/optimizer/strategy/create_fds.cpp
@@ -6,8 +6,9 @@ namespace opossum {
 
 void CreateFDsRule::apply_to(const std::shared_ptr<AbstractLQPNode>& lqp) const {
 
-  for(size_t i = 0; i < 100; ++i) {
-    const auto& fds = lqp->functional_dependencies();
+  for(size_t i = 0; i < 1000; ++i) {
+    Assert(!lqp->right_input(), "Did not expect right input node");
+    const auto& fds = lqp->left_input()->functional_dependencies();
     Assert(fds.size() > 0 || fds.size() == 0, "Dummy usage");
   }
 
--- src/lib/optimizer/strategy/create_unique_constraints.cpp
+++ src/lib/optimizer/strategy/create_unique_constraints.cpp
@@ -6,8 +6,9 @@ namespace opossum {
 
 void CreateUniqueConstraintsRule::apply_to(const std::shared_ptr<AbstractLQPNode>& lqp) const {
 
-  for(size_t i = 0; i < 100; ++i) {
-    const auto& unique_constraints = lqp->unique_constraints();
+  for(size_t i = 0; i < 1000; ++i) {
+    Assert(!lqp->right_input(), "Did not expect right input node");
+    const auto& unique_constraints = lqp->left_input()->unique_constraints();
     Assert(unique_constraints->size() > 0 || unique_constraints->size() == 0, "Dummy usage");
   }
 
